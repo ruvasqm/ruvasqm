5027b8dea9d1a90d4893744967d27131ddb633ee
Ruben Vasquez <ruvasqm@gmail.com>
2022-11-09 09:25:35 -0400

    fix resume api

--- pages/api/resume/cv_ruben_vasquez.ts
+++ pages/api/resume/cv_ruben_vasquez.ts
@@ -1,12 +1,20 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { Readable } from "stream";
+import resume from "@/resume.json";
+import fs from "fs";
+import path from "path";
+
+const filePath = path.resolve(
+  ".",
+  `public/cv_${resume.basics.name.replace(" ", "_").toLowerCase()}.pdf`
+);
+const pdf = fs.readFileSync(filePath);
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Readable | any>
 ) {
-  res.status(200).setHeader("Content-Type", "application/pdf");
-  Readable.from("@public/cv_ruben_vasquez.pdf").pipe(res);
+  res.status(200).setHeader("Content-Type", "application/pdf").send(pdf);
 }
 
 export const config = {
