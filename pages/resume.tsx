dae75a440fdaeb3156356ed629c630e5eec3f595
Ruben Vasquez <ruvasqm@gmail.com>
2022-11-09 14:50:45 -0400

    fix(resume): page-break using block for print

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -15,7 +15,7 @@ const Page: NextPage = () => {
     return (
     <>
         <SEO title='My Resume'/>
-        <main className='flex flex-col items-start min-h-auto print:h-auto w-full bg-secondary text-white p-5 md:p-14 space-y-3'>
+        <main className='flex flex-col items-start min-h-auto print:h-auto w-full bg-secondary text-white p-5 md:p-14 space-y-3 print:block'>
             <div className='flex flex-col lg:flex-row print:flex-row items-center w-full'>
                 <div className="flex flex-row gap-3 ">
                     <div className="relative rounded-full w-[10rem] h-[10rem] print:w-[5rem] print:h-[5rem]" title="me">
@@ -36,13 +36,13 @@ const Page: NextPage = () => {
                     ))}
                 </div>
             </div>
-            <section id='_about' className='flex flex-col items-start justify-start w-full sm:h-auto '>
+            <section id='_about' className='flex flex-col items-start justify-start w-full sm:h-auto print:block'>
                 <h1 className='text-2xl font-bold border-b-[0.3rem] border-primary p-3'>About me</h1>
                 <p className='text-base mt-2'>{resume.basics.summary}</p>
             </section>
-            <section id='_skills' className='flex flex-col items-start justify-start w-full h-fit'>
+            <section id='_skills' className='flex flex-col items-start justify-start w-full h-fit print:block'>
                 <h1 className='text-2xl font-bold border-b-[0.3rem] border-primary p-3'>Skills</h1>
-                <div className='flex flex-row flex-wrap w-full'>
+                <div className='flex flex-row flex-wrap w-full mt-2'>
                     {extractUniqueKeys(resume.skills, 'type').map((type: string) => (
                         <div key={type} className='flex flex-col items-start justify-start grow basis-1/3 md:p-3'>
                             <h2 className='text-xl font-bold'>{type}</h2>
@@ -55,18 +55,18 @@ const Page: NextPage = () => {
                     ))}
                 </div>
             </section>
-            <section id='_experience' className='flex flex-col items-start justify-start w-full h-fit'>
+            <section id='_experience' className='flex flex-col items-start justify-start w-full h-fit print:block'>
                 <h1 className='text-2xl font-bold border-b-[0.3rem] border-primary p-3'>Experience</h1>
-                <div className='flex flex-col items-start justify-start w-full h-full'>
+                <div className='flex flex-col items-start justify-start w-full h-full print:block'>
                     {resume.work.map((job) => (
-                        <div key={job.name} className='flex flex-col items-start justify-start w-full h-full p-3  print:break-inside-avoid-page'>
+                        <div key={job.name} className='flex flex-col items-start justify-start w-full h-full p-3  print:break-inside-avoid-page print:block'>
                             <div className='flex flex-row flex-wrap items-center w-full'>
                                 <h2 className='text-xl font-bold'>{job.name}</h2>
                                 <h3 className='text-base ml-3'>{job.position}</h3>
                                 <h5 className='text-xs font-bold ml-auto'>{job.startDate} - {job.endDate}</h5>
                             </div>
                             <a className='text-xs no-underline font-bold' href={job.url}>{job.url}</a>
-                            <p className='text-base p-2 print:break-inside-avoid-page'>{job.summary}</p>
+                            <p className='text-base p-2'>{job.summary}</p>
                             <div className='flex flex-row flex-wrap items-start justify-start w-full h-full'>
                                 {job.highlights.map((highlight) => pill(highlight))}
                             </div>
@@ -78,7 +78,7 @@ const Page: NextPage = () => {
                 <h1 className='text-2xl font-bold border-b-[0.3rem] border-primary p-3'>Education</h1>
                 <div className='flex flex-col items-start justify-start w-full h-full'>
                     {resume.education.map((school) => (
-                        <div key={school.institution} className='flex flex-col items-start justify-start w-full h-full p-3'>
+                        <div key={school.institution} className='flex flex-col items-start justify-start w-full h-full p-3 print:break-inside-avoid-page print:block'>
                             <div className='flex flex-row items-center w-full'>
                                 <h2 className='text-xl font-bold'>{school.institution}</h2>
                                 <h3 className='text-base ml-3'>{school.area}</h3>
@@ -93,7 +93,7 @@ const Page: NextPage = () => {
                 <h1 className='text-2xl font-bold border-b-[0.3rem] border-primary p-3'>Languages</h1>
                 <div className='flex flex-row flex-wrap items-start justify-start w-full h-full'>
                     {resume.languages.map((language) => (
-                        <div key={language.language} className='flex flex-col items-start justify-start p-3'>
+                        <div key={language.language} className='flex flex-col items-start justify-start p-3 print:break-inside-avoid-page print:block'>
                             <div className='flex flex-row items-center w-full p-2'>
                                 <h2 className='text-xl font-bold'>{language.language}</h2>
                                 <h3 className='text-base ml-3'>{language.fluency}</h3>
