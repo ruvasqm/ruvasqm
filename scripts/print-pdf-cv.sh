749d9cde1073d6bd216ee2d888ef0c1fa5f41cfb
Ruben Vasquez <ruvasqm@gmail.com>
2022-11-29 20:25:48 -0400

    chore: remove pdf scripts

--- scripts/print-pdf-cv.sh
+++
@@ -1,8 +0,0 @@
-yarn build
-yarn start &
-sleep 3
-node scripts/print-pdf.js 'http://localhost:3000/resume' './public/cv_ruben_vasquez.pdf'
-echo 'Shrinking PDF...'
-./scripts/shrinkpdf.sh './public/cv_ruben_vasquez.pdf' > 'out.pdf'
-mv -f 'out.pdf' './public/cv_ruben_vasquez.pdf'
-kill %1
